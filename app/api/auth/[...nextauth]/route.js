7373bfed5a478175723d9f3cdf03c142bfd466c2
sulthanahmadm-tech <sulthanahmadm@gmail.com>
2026-08-08 11:12:36 +0700

    fix: ubah import user jadi huruf U besar

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
-import User from "@/models/user";
+import User from "@/models/User";
 
 // Fungsi koneksi database (sesuaikan dengan file koneksimu)
 const connectDB = async () => {
